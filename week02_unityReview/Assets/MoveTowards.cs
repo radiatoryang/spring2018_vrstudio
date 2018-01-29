1c3ee52d490cdbc512321fe97acb0f064a73e2d4
Robert Yang <yang.robert.w@gmail.com>
2018-01-29 18:46:03 -0500

    week02: unity review (small 3D room; materials; moving objects with code via SimpleMoveScript.cs and MoveTowards.cs)

---
+++ week02_unityReview/Assets/MoveTowards.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// make cube move towards sphere...
+// put this script on the cube!
+public class MoveTowards : MonoBehaviour {
+
+	public Transform sphere; // "public" = exposes in inspector, and to other scripts
+
+	// Update is called once per frame
+	void Update () {
+		// calculate the vector from cube's current position to the sphere
+		Vector3 a = transform.position; // cube's position
+		Vector3 b = sphere.position; // sphere's position
+		Vector3 fromAtoB = b - a; // vector from a > b = b - a;
+
+		// normalize the vector so that it doesn't instantly move
+		if ( fromAtoB.magnitude > 1f ) { // but only if it's far away
+			fromAtoB = fromAtoB.normalized;
+			// fromAtoB = fromAtoB.Normalize(); // this does the same thing
+			// fromAtoB = fromAtoB / fromAtoB.magnitude; // this does the same thing too
+		}
+
+		// move the cube towards the sphere
+		transform.Translate( fromAtoB * Time.deltaTime, Space.World ); // "Translate" = move... Space.world = use worldspace
+	}
+}
---
+++ week02_unityReview/Assets/SimpleMoveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this makes the cube rise up and spin around
+// put this script on a Cube!
+public class SimpleMoveScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// move the cube upwards
+		// transform.position += new Vector3( 0f, 1f, 0f ); // move cube up 1m every frame
+
+		// better version: framerate INDEPENDENT that uses Time.deltaTime
+		transform.position += new Vector3(0f, 1f, 0f) * Time.deltaTime; // move up 1m per second
+
+		// we can make shapes spin too
+		transform.Rotate( 0f, 90f * Time.deltaTime, 0f ); // rotate 90 deg per second on Y axis
+	}
+}
