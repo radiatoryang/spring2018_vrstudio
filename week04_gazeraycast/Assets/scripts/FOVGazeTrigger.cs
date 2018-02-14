bcbde7cdb9972e48b9da5d306d1ccf0577b0f814
Robert Yang <yang.robert.w@gmail.com>
2018-02-14 17:26:18 -0500

    week04: GazeRaycast.cs + FOVGazeTrigger.cs example code

---
+++ week04_gazeraycast/Assets/scripts/FOVGazeTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// FOV = "field of view"
+// put this script on the object to be looked at
+// and this script will ask itself, "am I being looked at?"
+public class FOVGazeTrigger : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// 1. am I within the Camera's field of view? / middle of the screen?
+
+		// get vector from camera to object
+		Vector3 fromCameraToMe = transform.position - Camera.main.transform.position;
+
+		// compare that vector to camera's forward direction, and measure the angle
+		float angle = Vector3.Angle( Camera.main.transform.forward, fromCameraToMe.normalized );
+
+		// if that angle is less than X, then that object is within the vision cone
+		if( angle < 10f ) { // if within 10 degrees of middle of screen, then... etc etc
+			transform.Rotate( 0f, 90f * Time.deltaTime, 0f ); // test: rotate
+
+			// if you wanted to make sure there's nothing blocking our view,
+			// you would add a raycast check here, between Camera.main and this.transform
+			// (see GazeRaycast.cs for that kind of stuff)
+		}
+		
+	}
+}
---
+++ week04_gazeraycast/Assets/scripts/GazeRaycast.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this script will detect if you're looking at something
+// by shooting a raycast out the front of the camera
+// ... so put this script on your camera?
+public class GazeRaycast : MonoBehaviour {
+
+	float timeLookedAt;
+	
+	// Update is called once per frame
+	void Update () {
+		// SHOOTING A RAYCAST
+		// 1. construct a "Ray" object, to shoot out in front of camera
+		Ray myRay = new Ray( transform.position, transform.forward );
+
+		// 2. declare a RaycastHit object, to store the raycast hit info
+		RaycastHit myRayHit = new RaycastHit(); // this is a blank variable, will fill with data later
+
+		// 3. visualize the raycast in the editor
+		Debug.DrawRay( myRay.origin, myRay.direction * 100f, Color.yellow);
+
+		// 4. actually shoot the raycast now!
+		if( Physics.Raycast( myRay, out myRayHit, 100f ) ) {
+			timeLookedAt += Time.deltaTime; // after 1 second of looking, timeLookedAt == 1.0f
+
+			// once we hit something, we should spin it, to show that we're looking at it
+			if( timeLookedAt > 1f ) {
+				// myRayHit.transform = the thing the raycast hit
+				myRayHit.transform.Rotate( 0f, 90f * Time.deltaTime, 0f ); 
+			}
+		} else {
+			timeLookedAt = 0f; // reset the lookTimer if we're not looking at anything
+		}
+	}
+
+}
