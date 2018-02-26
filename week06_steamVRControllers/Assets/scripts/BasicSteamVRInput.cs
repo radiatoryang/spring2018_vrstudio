467984bc3889318e3d0d3fe0c7686bc514ce49df
Robert Yang <yang.robert.w@gmail.com>
2018-02-26 16:39:25 -0500

    week06a: VR controller sample code... BasicSteamVRInput.cs and MyButton.cs (uses Valve Interaction System)

---
+++ week06_steamVRControllers/Assets/scripts/BasicSteamVRInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// PUT THIS SCRIPT ON A CONTROLLER
+public class BasicSteamVRInput : MonoBehaviour {
+
+	// this is the basic script that lets SteamVR track an object's position / rotation
+	SteamVR_TrackedObject myTrackedObject;
+
+	// reference to the actual controller in SteamVR
+	SteamVR_Controller.Device myDevice { // this is a C# property, which is a var that can execute code
+		get { 
+			// tell SteamVR to treat the tracked object as a controller
+			return SteamVR_Controller.Input( (int)myTrackedObject.index );
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		myTrackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// is the user holding down the trigger on this controller?
+		if( myDevice.GetHairTrigger() ) {
+			Debug.Log( "user is holding down the trigger" );
+		}
+
+		// is the user holding down TOUCHPAD button, on this controller?
+		if( myDevice.GetPress( Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad ) ) {
+			Debug.Log( "user is holding down touchpad" );
+		}
+
+		// get touchpad axis position?
+		if( myDevice.GetAxis( Valve.VR.EVRButtonId.k_EButton_Axis0 ).x > 0f ) {
+
+		}
+	}
+}
---
+++ week06_steamVRControllers/Assets/scripts/MyButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Valve.VR.InteractionSystem; // you need this to code InteractionSystem scripts
+
+[RequireComponent(typeof(Interactable))] // forces us to have an Interactable script next to this script
+public class MyButton : MonoBehaviour {
+
+	// fires every frame as long as a hand (mouse or VR controller) is next to it
+	void HandHoverUpdate( Hand myHand ) {
+		// GetStandardInteractionButton == mouse click if in fallback mode, or trigger if in VR
+		if( myHand.GetStandardInteractionButtonDown() ) {
+			Debug.Log( "players clicked or triggered on me!" );
+			Debug.Log( myHand.transform.position );
+		}
+	}
+
+}
