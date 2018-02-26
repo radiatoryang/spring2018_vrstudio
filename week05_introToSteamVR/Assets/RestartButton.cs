fd969f92331861d9d8f428a920fcaaf7303ef16e
Robert Yang <yang.robert.w@gmail.com>
2018-02-26 16:39:06 -0500

    week05: intro to using SteamVR

---
+++ week05_introToSteamVR/Assets/RestartButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement; // lets me use SceneManager functions
+
+public class RestartButton : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// if you press [R] on keyboard, then reload the scene
+		if( Input.GetKeyDown( KeyCode.R ) ) {
+			// to actually restart the game, just reload the current scene
+			SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+			// IMPORTANT: don't forget to go to Window > Lighting > Settings, and uncheck the
+			// "Autogenerate Lighting" checkbox at the bottom, or else your scene will
+			// go "dark" when you reload the scene
+		}
+	}
+}
